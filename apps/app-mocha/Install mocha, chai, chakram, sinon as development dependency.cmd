c69b7f2fa1550c7cf7e39bae8072891e75eb68c8
teoman shipahi <aytekoz@yahoo.com>
2019-04-11 22:45:35 -0400

    files moved around

--- apps/app-mocha/Install mocha, chai, chakram, sinon as development dependency.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-::
-npm install mocha --save-dev 
-npm install chakram --save-dev
-npm install chai --save-dev
-npm install sinon --save-dev
-pause>nul
--- apps/app-rewire-winston-bluebird/npm init here.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-npm init
-pause>nul
